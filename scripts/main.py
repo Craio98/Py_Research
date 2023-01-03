e1b5ec3eddd11570bb9eb3f7236ebf783dcee658
Craio98 <116519294+Craio98@users.noreply.github.com>
2023-01-03 23:51:12 +0200

    Initialization, adding main.py

--- scripts/main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-
-fileName="https://github.com/Craio98/Py_Research/raw/main/graph.uhs"
-myCSV=pd.read_csv(fileName,skiprows=4)
-df=pd.DataFrame(myCSV)
-print(df)
